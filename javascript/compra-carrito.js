355290a9520927fe9f3617f072a0644e9e7e3e9c
DESKTOP-5OG7GUF\Admin <sergioarispevaz@gmail.com>
2018-11-21 16:46:17 -0300

    Avances en la ventana de compra.

--- javascript/compra-carrito.js
+++ javascript/compra-carrito.js
@@ -2,11 +2,15 @@ function compra(div) {
     var juegoLista = div.id;
     document.getElementById('nombre-compra').innerHTML = juegos[juegoLista].nombre;
     document.getElementById('precio-compra').innerHTML = juegos[juegoLista].precio;
+    document.getElementById('descuento-compra').innerHTML = juegos[juegoLista].descuento;
 
-    var x = document.getElementById("caja-compra");
-       if (x.style.display === "none") {
-           x.style.display = "block";
-       } else {
-           x.style.display = "none";
-       }
+    if (document.getElementById("caja-compra").style.display = "none") {
+           document.getElementById("caja-compra").style.display = "block";
+    }
+}
+
+function cerrarVentana() {
+  if (document.getElementById("caja-compra").style.display = "block") {
+    document.getElementById("caja-compra").style.display = "none";
+  }
 }
