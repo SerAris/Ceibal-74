723acc0beccbb478368473fead1fa1d55596da78
DESKTOP-5OG7GUF\Admin <sergioarispevaz@gmail.com>
2018-11-21 21:04:32 -0300

    Actualización de HTML, CSS y JS.

--- javascript/compra-carrito.js
+++ javascript/compra-carrito.js
@@ -3,6 +3,9 @@ function compra(div) {
     document.getElementById('nombre-compra').innerHTML = juegos[juegoLista].nombre;
     document.getElementById('precio-compra').innerHTML = juegos[juegoLista].precio;
     document.getElementById('descuento-compra').innerHTML = juegos[juegoLista].descuento;
+    if (juegos[juegoLista].descuento > 0) {
+      document.getElementById("ventana-descuento").style.display = "block";
+    } else {document.getElementById("ventana-descuento").style.display = "none";}
 
     if (document.getElementById("caja-compra").style.display = "none") {
            document.getElementById("caja-compra").style.display = "block";
