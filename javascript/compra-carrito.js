cf74669d0f4ef11f7f303506b69e69382d4294c2
DESKTOP-5OG7GUF\Admin <sergioarispevaz@gmail.com>
2018-11-21 23:55:44 -0300

    Actualiación.

--- javascript/compra-carrito.js
+++ javascript/compra-carrito.js
@@ -7,6 +7,22 @@ function compra(div) {
       document.getElementById("ventana-descuento").style.display = "block";
     } else {document.getElementById("ventana-descuento").style.display = "none";}
 
+    if (juegos[juegoLista].descuento > 0) {
+      document.getElementById("descuentoFinal").style.display = "block";
+    } else {document.getElementById("descuentoFinal").style.display = "none";}
+
+    if (juegos[juegoLista].descuento > 0) {
+      document.getElementById("precioFinalCuenta").style.display = "none";
+    } else {document.getElementById("precioFinalCuenta").style.display = "inline";}
+
+
+    var IVA = 1.22;
+    var input = document.querySelector('.cantidadAComprar');
+    var num = input.value;
+    var precioInicial = document.getElementById('precioJuego').innerHTML = juegos[juegoLista].precio;
+    var precioConIVA = document.getElementById('precioIVA').innerHTML = Math.round((precioInicial * IVA) * 10) / 10;
+    var precioConDesc = document.getElementById('precioDesc').innerHTML = Math.round((precioConIVA - ((precioConIVA * juegos[juegoLista].descuento)/100)) * num);
+
     if (document.getElementById("caja-compra").style.display = "none") {
            document.getElementById("caja-compra").style.display = "block";
     }
