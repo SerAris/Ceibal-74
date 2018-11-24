c24c87ddb3d7b56c7c97035b724260d41edfff87
DESKTOP-5OG7GUF\Admin <sergioarispevaz@gmail.com>
2018-11-24 04:51:44 -0300

    Corrección de código JS.

--- javascript/compra-carrito.js
+++ javascript/compra-carrito.js
@@ -1,35 +1,209 @@
-function compra(div) {
-    var juegoLista = div.id;
-    document.getElementById('nombre-compra').innerHTML = juegos[juegoLista].nombre;
-    document.getElementById('precio-compra').innerHTML = juegos[juegoLista].precio;
-    document.getElementById('descuento-compra').innerHTML = juegos[juegoLista].descuento;
-    if (juegos[juegoLista].descuento > 0) {
-      document.getElementById("ventana-descuento").style.display = "block";
-    } else {document.getElementById("ventana-descuento").style.display = "none";}
-
-    if (juegos[juegoLista].descuento > 0) {
-      document.getElementById("descuentoFinal").style.display = "block";
-    } else {document.getElementById("descuentoFinal").style.display = "none";}
-
-    if (juegos[juegoLista].descuento > 0) {
-      document.getElementById("precioFinalCuenta").style.display = "none";
-    } else {document.getElementById("precioFinalCuenta").style.display = "inline";}
-
-
-    var IVA = 1.22;
-    var input = document.querySelector('.cantidadAComprar');
-    var num = input.value;
-    var precioInicial = document.getElementById('precioJuego').innerHTML = juegos[juegoLista].precio;
-    var precioConIVA = document.getElementById('precioIVA').innerHTML = Math.round((precioInicial * IVA) * 10) / 10;
-    var precioConDesc = document.getElementById('precioDesc').innerHTML = Math.round((precioConIVA - ((precioConIVA * juegos[juegoLista].descuento)/100)) * num);
-
-    if (document.getElementById("caja-compra").style.display = "none") {
-           document.getElementById("caja-compra").style.display = "block";
-    }
+function abrirVentana() {
+  if (document.getElementById('caja-compra').style.display = "none") {
+    document.getElementById('caja-compra').style.display = "block"
+  }
 }
 
 function cerrarVentana() {
-  if (document.getElementById("caja-compra").style.display = "block") {
-    document.getElementById("caja-compra").style.display = "none";
+  if (document.getElementById('caja-compra').style.display = "block") {
+    document.getElementById('caja-compra').style.display = "none"
+    document.getElementById('formularioDeCompra').reset();
   }
+}
+
+$(document).ready(function() {
+	$("#formato-digital").click(function() {
+  	$("#metodoEnvio").attr("disabled", true);
+    $("#cantidadCompra").attr("disabled", true);
+    $("#direccionEnvio").attr("disabled", true);
+    $("#cantidadCompra").val(1);
+    $("#metodoEnvio").prop('selectedIndex', 0);
+    $("#selectMetodo").attr("value", "1");
+    $("#optionsCompra").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#formato-fisico").click(function() {
+    $("#metodoEnvio").attr("disabled", false);
+    $("#cantidadCompra").attr("disabled", false);
+    $("#direccionEnvio").attr("disabled", false);
+    $("#selectMetodo").attr("value", "");
+    $("#optionsCompra").show();
+  });
+})
+
+function compraExito() {
+  alert("¡Éxito! Tu compra se ha realizado satisfactoriamente.\n\nTe hemos enviado un correo electrónico con la constancia y factura de compra y los datos que nos proporcionaste.\n\n¡Muchas gracias!")
+}
+
+function compraJuego1() {
+  document.getElementById('formato-digital').setAttribute('onchange','compraJuego1();');
+  document.getElementById('formato-fisico').setAttribute('onchange','compraJuego1();');
+  document.getElementById('cantidadCompra').setAttribute('onchange','compraJuego1();');
+  document.getElementById('metodoEnvio').setAttribute('onchange','compraJuego1();');
+  document.getElementById("nombre-compra").innerHTML = juegos[0].nombre;
+  document.getElementById("DescPorcentaje").innerHTML = juegos[0].descuento;
+  var precioBase = juegos[0].precio;
+  var precioIVA = ((precioBase * iva)/100);
+  var precioDesc = ((precioBase + precioIVA) * juegos[0].descuento)/100;
+  var cantidadItem = document.getElementById("cantidadCompra").value;
+  var precioEnvio = document.getElementById("metodoEnvio").value;
+  var precioFinal = (precioBase + precioIVA - precioDesc) * cantidadItem * precioEnvio;
+  document.getElementById('precioBase').innerHTML = precioBase.toFixed(2);
+  document.getElementById('IVAdeJuego').innerHTML = precioIVA.toFixed(2);
+  document.getElementById('descuentoJuego').innerHTML = precioDesc.toFixed(2);
+  document.getElementById('precioFinal').innerHTML = precioFinal.toFixed(2);
+}
+
+function compraJuego2() {
+  document.getElementById('formato-digital').setAttribute('onchange','compraJuego2();');
+  document.getElementById('formato-fisico').setAttribute('onchange','compraJuego2();');
+  document.getElementById('cantidadCompra').setAttribute('onchange','compraJuego2();');
+  document.getElementById('metodoEnvio').setAttribute('onchange','compraJuego2();');
+  document.getElementById("nombre-compra").innerHTML = juegos[1].nombre;
+  document.getElementById("DescPorcentaje").innerHTML = juegos[1].descuento;
+  var precioBase = juegos[1].precio;
+  var precioIVA = ((precioBase * iva)/100);
+  var precioDesc = ((precioBase + precioIVA) * juegos[1].descuento)/100;
+  var cantidadItem = document.getElementById("cantidadCompra").value;
+  var precioEnvio = document.getElementById("metodoEnvio").value;
+  var precioFinal = (precioBase + precioIVA - precioDesc) * cantidadItem * precioEnvio;
+  document.getElementById('precioBase').innerHTML = precioBase.toFixed(2);
+  document.getElementById('IVAdeJuego').innerHTML = precioIVA.toFixed(2);
+  document.getElementById('descuentoJuego').innerHTML = precioDesc.toFixed(2);
+  document.getElementById('precioFinal').innerHTML = precioFinal.toFixed(2);
+}
+
+function compraJuego3() {
+  document.getElementById('formato-digital').setAttribute('onchange','compraJuego3();');
+  document.getElementById('formato-fisico').setAttribute('onchange','compraJuego3();');
+  document.getElementById('cantidadCompra').setAttribute('onchange','compraJuego3();');
+  document.getElementById('metodoEnvio').setAttribute('onchange','compraJuego3();');
+  document.getElementById("nombre-compra").innerHTML = juegos[2].nombre;
+  document.getElementById("DescPorcentaje").innerHTML = juegos[2].descuento;
+  var precioBase = juegos[2].precio;
+  var precioIVA = ((precioBase * iva)/100);
+  var precioDesc = ((precioBase + precioIVA) * juegos[2].descuento)/100;
+  var cantidadItem = document.getElementById("cantidadCompra").value;
+  var precioEnvio = document.getElementById("metodoEnvio").value;
+  var precioFinal = (precioBase + precioIVA - precioDesc) * cantidadItem * precioEnvio;
+  document.getElementById('precioBase').innerHTML = precioBase.toFixed(2);
+  document.getElementById('IVAdeJuego').innerHTML = precioIVA.toFixed(2);
+  document.getElementById('descuentoJuego').innerHTML = precioDesc.toFixed(2);
+  document.getElementById('precioFinal').innerHTML = precioFinal.toFixed(2);
+}
+
+function compraJuego4() {
+  document.getElementById('formato-digital').setAttribute('onchange','compraJuego4();');
+  document.getElementById('formato-fisico').setAttribute('onchange','compraJuego4();');
+  document.getElementById('cantidadCompra').setAttribute('onchange','compraJuego4();');
+  document.getElementById('metodoEnvio').setAttribute('onchange','compraJuego4();');
+  document.getElementById("nombre-compra").innerHTML = juegos[3].nombre;
+  document.getElementById("DescPorcentaje").innerHTML = juegos[3].descuento;
+  var precioBase = juegos[3].precio;
+  var precioIVA = ((precioBase * iva)/100);
+  var precioDesc = ((precioBase + precioIVA) * juegos[3].descuento)/100;
+  var cantidadItem = document.getElementById("cantidadCompra").value;
+  var precioEnvio = document.getElementById("metodoEnvio").value;
+  var precioFinal = (precioBase + precioIVA - precioDesc) * cantidadItem * precioEnvio;
+  document.getElementById('precioBase').innerHTML = precioBase.toFixed(2);
+  document.getElementById('IVAdeJuego').innerHTML = precioIVA.toFixed(2);
+  document.getElementById('descuentoJuego').innerHTML = precioDesc.toFixed(2);
+  document.getElementById('precioFinal').innerHTML = precioFinal.toFixed(2);
+}
+
+function compraJuego5() {
+  document.getElementById('formato-digital').setAttribute('onchange','compraJuego5();');
+  document.getElementById('formato-fisico').setAttribute('onchange','compraJuego5();');
+  document.getElementById('cantidadCompra').setAttribute('onchange','compraJuego5();');
+  document.getElementById('metodoEnvio').setAttribute('onchange','compraJuego5();');
+  document.getElementById("nombre-compra").innerHTML = juegos[4].nombre;
+  document.getElementById("DescPorcentaje").innerHTML = juegos[4].descuento;
+  var precioBase = juegos[4].precio;
+  var precioIVA = ((precioBase * iva)/100);
+  var precioDesc = ((precioBase + precioIVA) * juegos[4].descuento)/100;
+  var cantidadItem = document.getElementById("cantidadCompra").value;
+  var precioEnvio = document.getElementById("metodoEnvio").value;
+  var precioFinal = (precioBase + precioIVA - precioDesc) * cantidadItem * precioEnvio;
+  document.getElementById('precioBase').innerHTML = precioBase.toFixed(2);
+  document.getElementById('IVAdeJuego').innerHTML = precioIVA.toFixed(2);
+  document.getElementById('descuentoJuego').innerHTML = precioDesc.toFixed(2);
+  document.getElementById('precioFinal').innerHTML = precioFinal.toFixed(2);
+}
+
+function compraJuego6() {
+  document.getElementById('formato-digital').setAttribute('onchange','compraJuego6();');
+  document.getElementById('formato-fisico').setAttribute('onchange','compraJuego6();');
+  document.getElementById('cantidadCompra').setAttribute('onchange','compraJuego6();');
+  document.getElementById('metodoEnvio').setAttribute('onchange','compraJuego6();');
+  document.getElementById("nombre-compra").innerHTML = juegos[5].nombre;
+  document.getElementById("DescPorcentaje").innerHTML = juegos[5].descuento;
+  var precioBase = juegos[5].precio;
+  var precioIVA = ((precioBase * iva)/100);
+  var precioDesc = ((precioBase + precioIVA) * juegos[5].descuento)/100;
+  var cantidadItem = document.getElementById("cantidadCompra").value;
+  var precioEnvio = document.getElementById("metodoEnvio").value;
+  var precioFinal = (precioBase + precioIVA - precioDesc) * cantidadItem * precioEnvio;
+  document.getElementById('precioBase').innerHTML = precioBase.toFixed(2);
+  document.getElementById('IVAdeJuego').innerHTML = precioIVA.toFixed(2);
+  document.getElementById('descuentoJuego').innerHTML = precioDesc.toFixed(2);
+  document.getElementById('precioFinal').innerHTML = precioFinal.toFixed(2);
+}
+
+function compraJuego7() {
+  document.getElementById('formato-digital').setAttribute('onchange','compraJuego7();');
+  document.getElementById('formato-fisico').setAttribute('onchange','compraJuego7();');
+  document.getElementById('cantidadCompra').setAttribute('onchange','compraJuego7();');
+  document.getElementById('metodoEnvio').setAttribute('onchange','compraJuego7();');
+  document.getElementById("nombre-compra").innerHTML = juegos[6].nombre;
+  document.getElementById("DescPorcentaje").innerHTML = juegos[6].descuento;
+  var precioBase = juegos[6].precio;
+  var precioIVA = ((precioBase * iva)/100);
+  var precioDesc = ((precioBase + precioIVA) * juegos[6].descuento)/100;
+  var cantidadItem = document.getElementById("cantidadCompra").value;
+  var precioEnvio = document.getElementById("metodoEnvio").value;
+  var precioFinal = (precioBase + precioIVA - precioDesc) * cantidadItem * precioEnvio;
+  document.getElementById('precioBase').innerHTML = precioBase.toFixed(2);
+  document.getElementById('IVAdeJuego').innerHTML = precioIVA.toFixed(2);
+  document.getElementById('descuentoJuego').innerHTML = precioDesc.toFixed(2);
+  document.getElementById('precioFinal').innerHTML = precioFinal.toFixed(2);
+}
+
+function compraJuego8() {
+  document.getElementById('formato-digital').setAttribute('onchange','compraJuego8();');
+  document.getElementById('formato-fisico').setAttribute('onchange','compraJuego8();');
+  document.getElementById('cantidadCompra').setAttribute('onchange','compraJuego8();');
+  document.getElementById('metodoEnvio').setAttribute('onchange','compraJuego8();');
+  document.getElementById("nombre-compra").innerHTML = juegos[7].nombre;
+  document.getElementById("DescPorcentaje").innerHTML = juegos[7].descuento;
+  var precioBase = juegos[7].precio;
+  var precioIVA = ((precioBase * iva)/100);
+  var precioDesc = ((precioBase + precioIVA) * juegos[7].descuento)/100;
+  var cantidadItem = document.getElementById("cantidadCompra").value;
+  var precioEnvio = document.getElementById("metodoEnvio").value;
+  var precioFinal = (precioBase + precioIVA - precioDesc) * cantidadItem * precioEnvio;
+  document.getElementById('precioBase').innerHTML = precioBase.toFixed(2);
+  document.getElementById('IVAdeJuego').innerHTML = precioIVA.toFixed(2);
+  document.getElementById('descuentoJuego').innerHTML = precioDesc.toFixed(2);
+  document.getElementById('precioFinal').innerHTML = precioFinal.toFixed(2);
+}
+
+function compraJuego9() {
+  document.getElementById('formato-digital').setAttribute('onchange','compraJuego9();');
+  document.getElementById('formato-fisico').setAttribute('onchange','compraJuego9();');
+  document.getElementById('cantidadCompra').setAttribute('onchange','compraJuego9();');
+  document.getElementById('metodoEnvio').setAttribute('onchange','compraJuego9();');
+  document.getElementById("nombre-compra").innerHTML = juegos[8].nombre;
+  document.getElementById("DescPorcentaje").innerHTML = juegos[8].descuento;
+  var precioBase = juegos[8].precio;
+  var precioIVA = ((precioBase * iva)/100);
+  var precioDesc = ((precioBase + precioIVA) * juegos[8].descuento)/100;
+  var cantidadItem = document.getElementById("cantidadCompra").value;
+  var precioEnvio = document.getElementById("metodoEnvio").value;
+  var precioFinal = (precioBase + precioIVA - precioDesc) * cantidadItem * precioEnvio;
+  document.getElementById('precioBase').innerHTML = precioBase.toFixed(2);
+  document.getElementById('IVAdeJuego').innerHTML = precioIVA.toFixed(2);
+  document.getElementById('descuentoJuego').innerHTML = precioDesc.toFixed(2);
+  document.getElementById('precioFinal').innerHTML = precioFinal.toFixed(2);
 }
